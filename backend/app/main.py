8cbfe2c3bfee32107e67f15002e0eb82359e9e14
abdulkasif <abdulkasif95@gmail.com>
2025-08-03 23:45:58 +0530

    chore(backend): set up dependency to get DB session

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from . import models, database
+
+models.base.metadata.create_all(bind=database.engine)
+
+# Dependency to get db session
+def get_db():
+    db = database.sessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 app = FastAPI()
 
