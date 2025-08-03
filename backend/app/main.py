10141d230c8033d2309c0e322b3e13907d39491f
abdulkasif <abdulkasif95@gmail.com>
2025-08-04 00:10:15 +0530

    feat(backend): add create_task API route logic

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,7 +4,7 @@ from . import models, database
 
 models.base.metadata.create_all(bind=database.engine)
 
-# Dependency to get db session
+# Dependency to get DB session
 def get_db():
     db = database.sessionLocal()
     try:
@@ -13,6 +13,14 @@ def get_db():
         db.close()
 
 app = FastAPI()
+
+@app.post("/tasks")
+def create_task(title: str, description: str = "", db: Session = Depends(get_db)):
+    task = models.Task(title=title, description=description)
+    db.add(task)
+    db.commit()
+    db.refresh(task)
+    return task
 
 @app.get("/")
 def read_roots():
